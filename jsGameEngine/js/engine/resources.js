ff53c82e4fb5042e8d3d81ee534e51a138570d63
SFuhad <166405754+SFuhad@users.noreply.github.com>
2024-12-11 12:21:53 +0000

    Attempt to create UI and add a player
    This commit contains an attempt to add some platforms and a player into the game.

--- jsGameEngine/js/engine/resources.js
+++ jsGameEngine/js/engine/resources.js
@@ -2,8 +2,17 @@
 const Images = {
   player: new Image(), // The Image instance for the player.
   enemy: new Image(),
-  collectible: new Image()// The Image instance for the enemy.
+  collectible: new Image(),// The Image instance for the enemy.
+  projectile1: new Image(),
 };
+
+const RunImages = 
+        [
+            new Image(), // The Image instance for the player.
+            new Image(),
+            new Image(),// The Image instance for the enemy.
+            new Image(),
+        ];
 
 // Create an AudioFiles object to hold the file paths of the audio resources.
 const AudioFiles = {
@@ -13,10 +22,18 @@ const AudioFiles = {
 };
 
 // Set the source of the player image.
-Images.player.src = './resources/images/player/Santa.png'; // Update the image path
+Images.player.src = './resources/images/player/player.png'; // Update the image path
 
 // Set the source of the enemy image.
 Images.enemy.src = './resources/images/enemy/enemy.png'; // Update the image path
 Images.collectible.src = "./resources/images/collectible/star.png";
+Images.projectile1.src = "./resources/images/player/Snowball.png";
+
+RunImages[0].src = "./resources/images/player/Run1.png";
+RunImages[1].src = "./resources/images/player/Run2.png";
+RunImages[2].src = "./resources/images/player/Run3.png";
+RunImages[3].src = "./resources/images/player/Run4.png";
+
+
 // Export the Images and AudioFiles objects so they can be imported and used in other modules.
-export { Images, AudioFiles };
+export { Images, AudioFiles, RunImages };
